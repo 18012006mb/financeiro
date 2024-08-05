3534acd48dc10fe18e8ae41c0919cab27a3263f5
18012006mb <160506109+18012006mb@users.noreply.github.com>
2024-08-05 11:50:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const caixaPrincipal= document.querySelector(".caixa-principal");
+const caixaPerguntas= documento.querySelector(".caixa-perguntas");
+const caixaAlternativas= document.querySelector("caixa-resultado");
+constResultado= document.querySelector("caixa-resultado");
+const textoResultado= document.querySelector(".texto-resultado");
+
+const perguntas=[
+{
+    enunciado:"Minha família está passandio por situações financeiras,como resolver?"
+    a1ternativas:[
+        {
+          texto:"Orçamento Detalhado"  
+          afirmacao:"Permite indentificar despesas excessivas,permetindo cortar gastos desnecessários"
+        },
+        {
+         texto:"Priorização de gastos"
+         afirmacao: "Indentificar e priorizar as despesas essenciais"
+        }
+     ]
+},
+{
+  enunciado:"Como o orçamento detalhado pode ajudar na situação financeira?"
+  alternativas:[
+    {
+        texto:
+    }
+  ]
+}
+]
